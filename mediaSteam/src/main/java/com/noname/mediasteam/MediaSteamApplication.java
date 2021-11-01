86d6cdae405c27feb348f8a87784e50584d1d990
DESKTOP-I9LII5C\izbean <gjslgood2@gmail.com>
2021-11-01 14:58:49 +0900

    initialize commit

---
+++ mediaSteam/src/main/java/com/noname/mediasteam/MediaSteamApplication.java
@@ -0,0 +1,13 @@
+package com.noname.mediasteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaSteamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaSteamApplication.class, args);
+    }
+
+}
---
+++ mediaSteam/src/test/java/com/noname/mediasteam/MediaSteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noname.mediasteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaSteamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
