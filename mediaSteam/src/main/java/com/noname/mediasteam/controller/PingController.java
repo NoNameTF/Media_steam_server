92e1349fabcbf6c6a7d0ca252a734d763ed82158
izbean <gjslgood2@gmail.com>
2021-11-09 21:03:47 +0900

    add-ping-controller

---
+++ mediaSteam/src/main/java/com/noname/mediasteam/controller/PingController.java
@@ -0,0 +1,16 @@
+package com.noname.mediasteam.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/ping")
+public class PingController {
+
+    @GetMapping
+    public String ping() {
+        return "pong";
+    }
+
+}
