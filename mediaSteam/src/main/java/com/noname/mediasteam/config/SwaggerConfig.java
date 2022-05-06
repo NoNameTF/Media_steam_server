7b9f4eee1efbc5783e8a11e4dfe97fb7dcbd1c65
강병헌 <bhkang@wavve.com>
2022-05-07 03:11:22 +0900

    feat: add swagger ui

---
+++ mediaSteam/src/main/java/com/noname/mediasteam/config/SwaggerConfig.java
@@ -0,0 +1,48 @@
+package com.noname.mediasteam.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.noname.mediasteam.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .securityContexts(List.of(securityContext()))
+                .securitySchemes(List.of(apiKey()));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .operationSelector(operationContext -> true)
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[]{authorizationScope};
+
+        return List.of(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+}
